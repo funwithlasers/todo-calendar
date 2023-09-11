0c7c07294311fb506288344e608ce3249c657c16
funwithlasers <mjlarson13@gmail.com>
2023-09-11 14:58:42 -0500

    pass function to component and remove div

--- src/components/ChoreCreate.js
+++ src/components/ChoreCreate.js
@@ -6,6 +6,7 @@ import CreateIcon from './icons/CreateIcon';
 function ChoreCreate({ date }) {
   const [title, setTitle] = useState('');
   const { createChore } = useChoresContext();
+  const [isCreateChore, setIsCreateChore] = useState(false);
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -15,16 +16,31 @@ function ChoreCreate({ date }) {
     event.preventDefault();
     createChore(title, 0, date);
     setTitle('');
+    setIsCreateChore(false);
+  };
+
+  const handleModal = () => {
+    console.log('handleModal triggered');
+    setIsCreateChore(!isCreateChore);
+  }
+
+  let modal = () => {
+
+    if (isCreateChore) {
+      return (
+        <form onSubmit={handleSubmit}>
+          <label>Title</label>
+          <input className='input' value={title} onChange={handleChange} />
+          <button className='button'>Create!</button>
+        </form>
+      );
+    }
   };
 
   return (
-    <div className='chore-create'>
-      <CreateIcon />
-      <form onSubmit={handleSubmit}>
-        <label>Title</label>
-        <input className='input' value={title} onChange={handleChange} />
-        <button className='button'>Create!</button>
-      </form>
+    <div className='chore-create' >
+      <CreateIcon onClick={handleModal} />
+      {modal()}
     </div>
   );
 }
--- src/components/icons/CreateIcon.js
+++ src/components/icons/CreateIcon.js
@@ -1,11 +1,11 @@
 import { BiSolidPlusCircle } from 'react-icons/bi';
 import { IconContext } from 'react-icons';
 
-function CreateIcon() {
+function CreateIcon({ onClick }) {
   return (
     <IconContext.Provider value={{ color: 'deepskyblue', className: 'complete-icon' }}>
       <div>
-        <BiSolidPlusCircle />
+        <BiSolidPlusCircle onClick={onClick}/>
       </div>
     </IconContext.Provider>
   );

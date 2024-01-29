494454773356465090b87c5b13b682ec36e2dc36
funwithlasers <mjlarson13@gmail.com>
2024-01-29 11:03:00 -0600

    set active day on click

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import ChoreCreate from './ChoreCreate';
 import ChoreList from './ChoreList';
 import DayContext from '../context/days';
+import { toDate } from 'date-fns';
 
 
 function Day({ date }) {
@@ -9,9 +10,8 @@ function Day({ date }) {
 
   return (
     <div className='h-screen'>
-      <div className={'relative border-solid border-black border-2 pb-5 h-4/5 ' + (activeDay.toLocaleDateString("en-US") === date ? 'border-cyan-600' : 'border-black')}>
+      <div onClick={()=>{setActiveDay(toDate(date))}} className={'relative border-solid border-black border-2 pb-5 h-4/5 ' + (activeDay.toLocaleDateString("en-US") === date ? 'border-cyan-600' : 'border-black')}>
         <h1 className='text-3xl font-bold text-white border-b-2 border-black bg-black'>{date}</h1>
-        <p>{activeDay.toLocaleDateString("en-US")}</p>
         <ChoreList date={date} />
         <div>
           <ChoreCreate date={date} />

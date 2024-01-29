ec8f32192b4ee03297705350d092e4e58a76ba03
funwithlasers <mjlarson13@gmail.com>
2024-01-29 11:11:01 -0600

    update formatting

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,7 +2,7 @@ import { useContext, useEffect } from 'react';
 import ChoreCreate from './ChoreCreate';
 import ChoreList from './ChoreList';
 import DayContext from '../context/days';
-import { toDate } from 'date-fns';
+import { formatDate, toDate } from 'date-fns';
 
 
 function Day({ date }) {
@@ -11,7 +11,8 @@ function Day({ date }) {
   return (
     <div className='h-screen'>
       <div onClick={()=>{setActiveDay(toDate(date))}} className={'relative border-solid border-black border-2 pb-5 h-4/5 ' + (activeDay.toLocaleDateString("en-US") === date ? 'border-cyan-600' : 'border-black')}>
-        <h1 className='text-3xl font-bold text-white border-b-2 border-black bg-black'>{date}</h1>
+      <h1 className='text-3xl font-bold text-white border-b-2 border-black bg-black'>{formatDate(date, 'EEEE')}</h1>
+      <h1 className='text-3xl font-bold text-white border-b-2 border-black bg-black'>{formatDate(date, 'MMMM')} {formatDate(date, 'do')}</h1>
         <ChoreList date={date} />
         <div>
           <ChoreCreate date={date} />

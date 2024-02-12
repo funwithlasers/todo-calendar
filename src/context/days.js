83f12cc4f71a372c970fea5b81f1d1172d3be7dc
funwithlasers <mjlarson13@gmail.com>
2024-02-12 09:36:31 -0600

    dates are dates

--- src/context/days.js
+++ src/context/days.js
@@ -5,7 +5,7 @@ const DaysContext = createContext();
 
 function DaysProvider({ children }) {
   const [selectedDay, setSelectedDay] = useState(startOfDay(new Date()));
-  var dayInScope = selectedDay;
+  const [dayInScope, setDayInScope] = useState(selectedDay);
 
   function getWeekDays(date) {
     var weekdays = [...Array(7)];
@@ -19,40 +19,25 @@ function DaysProvider({ children }) {
     return weekdays;
   }
 
-  const [days, setDays] = useState(getWeekDays());
+  const [days, setDays] = useState(getWeekDays(dayInScope));
 
-  function incrementSelectedDay() {
-    var next = addDays(selectedDay, 1);
-    setDays(getWeekDays(next));
-    setSelectedDay(next);
-  }
-
-  function decrementSelectedDay() {
-    var prev = subDays(selectedDay, 1);
-    setDays(getWeekDays(prev));
-    setSelectedDay(prev);
-  }
-
-
-  //Always one week because no adjust active day
   function incrementWeek() {
-    var next = addDays(dayInScope, 7);
+    const next = addDays(dayInScope, 7);
+    setDayInScope(next);
     setDays(getWeekDays(next));
   }
 
   function decrementWeek() {
-    var prev = subDays(dayInScope, 7);
+    const prev = subDays(dayInScope, 7);
+    setDayInScope(prev);
     setDays(getWeekDays(prev));
   }
-
 
   const daysContext = {
     days,
     selectedDay,
     setSelectedDay,
     getWeekDays,
-    incrementSelectedDay,
-    decrementSelectedDay,
     incrementWeek,
     decrementWeek
   };
